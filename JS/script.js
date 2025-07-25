63e774ef7bb0c86ab6bdadfb0fa4b1f42fb1e122
Jay Patel <jay1.worldweb@gmail.com>
2025-07-25 17:45:02 +0530

    footer

---
+++ JS/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+ 
+  $('#header-placeholder').load('/header.html', function () {
+    
+    const currentPage = window.location.pathname.split("/").pop();
+    $(".nav-links a").each(function () {
+      const href = $(this).attr("href");
+      if (href === currentPage || (href === "index.html" && currentPage === "")) {
+        $(this).addClass("active");
+      }
+    });
+
+    $('.search').click(function (e) {
+      e.stopPropagation();
+      $('.search-container').toggleClass('active');
+    });
+
+    $(document).click(function (e) {
+      if (!$(e.target).closest('.search-container').length) {
+        $('.search-container').removeClass('active');
+      }
+    });
+
+   
+    $('.menu-toggle').click(function (e) {
+      e.stopPropagation();
+      $('.nav-links').toggleClass('active');
+      $(this).find('i').toggleClass('fa-bars fa-xmark');
+    });
+
+    $(document).click(function (e) {
+      if (!$(e.target).closest('.nav-links, .menu-toggle').length) {
+        $('.nav-links').removeClass('active');
+        $('.menu-toggle i').removeClass('fa-xmark').addClass('fa-bars');
+      }
+    });
+
+  }); // end of .load()
+});
